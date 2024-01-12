b3d75e6296ad2c813f056c4cb3314e64b2e29d7a
AmulyaMachaganti <133393249+AmulyaMachaganti@users.noreply.github.com>
2024-01-12 10:37:14 +0530

    Update purple.py

--- purple.py
+++ purple.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('Fairy')
 st.write('Fairy')
-st.image('https://wallpaperset.com/w/full/5/f/d/136189.jpg',caption='Fairy')
+st.image('https://youtu.be/PbPODDfgQMA?si=XvxKtETXbaa6p1-n',caption='Fairy')
