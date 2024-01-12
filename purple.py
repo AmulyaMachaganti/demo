4c232ca6328cf05501f882e94c5c64d87d6f2226
AmulyaMachaganti <133393249+AmulyaMachaganti@users.noreply.github.com>
2024-01-12 10:38:55 +0530

    Update purple.py

--- purple.py
+++ purple.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('Deepthi')
 st.write('Deepthi')
-st.image('https://youtu.be/PbPODDfgQMA?si=XvxKtETXbaa6p1-n',caption='Emone')
+st.video('https://youtu.be/PbPODDfgQMA?si=XvxKtETXbaa6p1-n',caption='Emone')
