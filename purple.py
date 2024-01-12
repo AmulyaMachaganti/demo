c56c29ea6e6848d53a4418f3f32f728c5a8d99c7
AmulyaMachaganti <133393249+AmulyaMachaganti@users.noreply.github.com>
2024-01-12 10:50:05 +0530

    Update purple.py

--- purple.py
+++ purple.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.header('Fairy')
-st.write('Fairy')
+st.write('Fly beyond the limits')
 st.image('https://wallpaperset.com/w/full/5/f/d/136189.jpg',caption='Fairy')
 st.header('Deepthi')
-st.write('Deepthi')
+st.write('Tattukolede')
 st.video('https://youtu.be/u_2ariWW8K0?si=IayA8WMnXZltcSHC')
